f8595cc4adf4aab16e19027d5e562df043f81c02
taotao257 <tomliu246@gmail.com>
2024-07-03 15:19:28 +0800

    vue0703

---
+++ my-vue-project/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>父組建中接收到的數據:{{ childData }}</h1>
+    <HelloWorld 
+      msg="Welcome to Your Vue.js App"
+      :count="parentCount"
+      @child-count-change="handler"
+      
+    >this is default content 1
+    <template #footer="{childCount}">first footer{{ childCount }}</template>
+    </HelloWorld>
+    <HelloWorld>this is default content 2</HelloWorld>
+    <HelloWorld>this is default content 3</HelloWorld>
+    <HelloWorld></HelloWorld>
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      parentCount: 1000,
+      childData: 0
+    }
+  },
+  methods:{
+    handler(childCount){
+      this.childData = childCount
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <slot>基礎的默認內容</slot>
+    <h1>{{ msg }}</h1>
+    <p>count in props{{ count }}</p>
+    <button @click="handler">按鈕</button>
+    <slot name="footer" :childCount="childCount">footer的默認內容</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  //組件通信 
+  //1. 父傳子 通過 props 進行處理
+  //2. 子傳子 過自定義事件處理
+  props: {
+    msg: String,
+    count:{
+      type:[String, Number],
+      //default:100
+      required: true
+    }
+  },
+  data(){
+    return{
+      childCount:0
+    }
+  },
+  methods:{
+    handler(){
+      this.childCount++
+      this.$emit('child-count-change', this.childCount)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
